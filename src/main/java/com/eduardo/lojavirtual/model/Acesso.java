f39fa76c1d583fbbf9b5f4f3dbf1d056199db99f
Eduardo Conceição <eduardosaconceicao@gmail.com>
2023-03-24 10:12:02 -0300

    Criada classe Acesso

---
+++ src/main/java/com/eduardo/lojavirtual/model/Acesso.java
@@ -0,0 +1,26 @@
+package com.eduardo.lojavirtual.model;
+
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "acesso")
+@SequenceGenerator(name = "seq_acesso", sequenceName = "seq_acesso", initialValue = 1, allocationSize = 1)
+public class Acesso implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_acesso")
+    private Long id;
+
+    @Column(nullable = false)
+    private String descricao; /* Acesso, ex.: ROLE_ADMIN ou ROLE_SECRETARIO */
+
+    @Override
+    public String getAuthority() {
+        return this.descricao;
+    }
+
+}
