37fd6f2e3afaa8300de7eefe297962428cc27d63
Eduardo Conceição <eduardosaconceicao@gmail.com>
2023-04-03 22:20:07 -0300

    Metodo salvarAcesso no padrao REST e criado classe Security para liberar END-POINT

--- src/main/java/com/eduardo/lojavirtual/controller/AcessoController.java
+++ src/main/java/com/eduardo/lojavirtual/controller/AcessoController.java
@@ -3,9 +3,9 @@ package com.eduardo.lojavirtual.controller;
 import com.eduardo.lojavirtual.model.Acesso;
 import com.eduardo.lojavirtual.service.AcessoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/acesso")
@@ -14,8 +14,10 @@ public class AcessoController {
     @Autowired
     private AcessoService acessoService;
 
-    @PostMapping
-    public Acesso salvarAcesso(Acesso acesso){
-        return acessoService.save(acesso);
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(value = "/salvarAcesso") /* Mapeando a url para receber JSON */
+    public ResponseEntity<Acesso> salvarAcesso(@RequestBody Acesso acesso){ /* Recebe o JSON e converte para Objeto*/
+        Acesso acessoSalvo = acessoService.save(acesso);
+        return ResponseEntity.ok().body(acessoSalvo);
     }
 }
