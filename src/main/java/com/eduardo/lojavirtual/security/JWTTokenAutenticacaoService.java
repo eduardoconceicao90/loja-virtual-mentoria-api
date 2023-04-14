d438046812a9671af8891eedec8cfb97f8979c49
Eduardo Conceição <eduardosaconceicao@gmail.com>
2023-04-13 21:04:01 -0300

    Adicionando logica para liberacao de cors

--- src/main/java/com/eduardo/lojavirtual/security/JWTTokenAutenticacaoService.java
+++ src/main/java/com/eduardo/lojavirtual/security/JWTTokenAutenticacaoService.java
@@ -35,7 +35,29 @@ public class JWTTokenAutenticacaoService {
         /* Dá resposta para tela e para o cliente (outra API, navegar, aplicativo, etc.) */
         response.addHeader(HEADER_STRING, token);
 
+        libercaoCors(response);
+
         /* Usado para ver no Postman para teste */
         response.getWriter().write("{\"Authorization\": \"" + token + "\"}");
+    }
+
+    /* Fazendo liberação contra erro de Cors no navegador */
+    private void libercaoCors(HttpServletResponse response){
+
+        if (response.getHeader("Access-Control-Allow-Origin") == null) {
+            response.addHeader("Access-Control-Allow-Origin", "*");
+        }
+
+        if (response.getHeader("Access-Control-Allow-Headers") == null) {
+            response.addHeader("Access-Control-Allow-Headers", "*");
+        }
+
+        if (response.getHeader("Access-Control-Request-Headers") == null) {
+            response.addHeader("Access-Control-Request-Headers", "*");
+        }
+
+        if (response.getHeader("Access-Control-Allow-Methods") == null) {
+            response.addHeader("Access-Control-Allow-Methods", "*");
+        }
     }
 }
