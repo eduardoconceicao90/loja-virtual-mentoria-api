51522cd1005c6ec28b13d17201f44542f9115daa
Eduardo Conceição <eduardosaconceicao@gmail.com>
2023-04-13 21:59:45 -0300

    Configuracao de no WebConfigSecurity para ativar a segurança

--- src/main/java/com/eduardo/lojavirtual/security/WebConfigSecurity.java
+++ src/main/java/com/eduardo/lojavirtual/security/WebConfigSecurity.java
@@ -6,10 +6,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import javax.servlet.http.HttpSessionListener;
 
@@ -21,16 +25,37 @@ public class WebConfigSecurity extends WebSecurityConfigurerAdapter implements H
     @Autowired
     private ImplUserDetailsService implUserDetailsService;
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .disable().authorizeRequests().antMatchers("/").permitAll()
+                .antMatchers("/index").permitAll()
+                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+
+                /* Redireciona ou da um retorno para index quando desloga */
+                .anyRequest().authenticated().and().logout().logoutSuccessUrl("/index")
+
+                /* Mapeia o logout do sistema */
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+
+                /* Filtra as requisicoes para login de JWT */
+                .and().addFilterAfter(new JWTLoginFilter("/login", authenticationManager()),
+                        UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(new JwtApiAutenticacaoFilter(), UsernamePasswordAuthenticationFilter.class);
+    }
+
     /* Irá consultar o user no banco com Spring Security */
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(implUserDetailsService).passwordEncoder(new BCryptPasswordEncoder());
     }
 
+    /* Ignora alguas URL livre de autenticação */
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers(HttpMethod.GET, "/acesso/**")
-                .antMatchers(HttpMethod.POST, "/acesso/**")
-                .antMatchers(HttpMethod.DELETE, "/acesso/**");
+                  //web.ignoring().antMatchers(HttpMethod.GET, "/acesso/**")
+                  //.antMatchers(HttpMethod.POST, "/acesso/**")
+                  //.antMatchers(HttpMethod.DELETE, "/acesso/**");
     }
 }
