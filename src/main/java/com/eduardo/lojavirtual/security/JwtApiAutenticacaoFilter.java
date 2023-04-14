9e6784870e2883610ac645a6470ea39d1d423f0a
Eduardo Conceição <eduardosaconceicao@gmail.com>
2023-04-13 21:47:02 -0300

    Criado o JwtApiAutenticacaoFilter

---
+++ src/main/java/com/eduardo/lojavirtual/security/JwtApiAutenticacaoFilter.java
@@ -0,0 +1,31 @@
+package com.eduardo.lojavirtual.security;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/* Filtro onde todas as requisicoes serão capturadas para autenticar */
+public class JwtApiAutenticacaoFilter extends GenericFilterBean {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+        /* Estabelece a autenticao do user */
+        Authentication authentication = new JWTTokenAutenticacaoService().
+                getAuthetication((HttpServletRequest) request, (HttpServletResponse) response);
+
+        /* Coloca o processo de autenticacao para o spring security */
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        chain.doFilter(request, response);
+    }
+
+}
