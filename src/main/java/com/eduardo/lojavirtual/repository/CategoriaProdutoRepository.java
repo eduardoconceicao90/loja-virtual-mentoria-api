f4ecb4ac1d65c700a72946b7479755c71dfb4e6e
Eduardo Conceição <eduardosaconceicao@gmail.com>
2023-05-02 21:20:24 -0300

    Ajustes

--- src/main/java/com/eduardo/lojavirtual/repository/CategoriaProdutoRepository.java
+++ src/main/java/com/eduardo/lojavirtual/repository/CategoriaProdutoRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 @Repository
 public interface CategoriaProdutoRepository extends JpaRepository<CategoriaProduto, Long> {
 
-    @Query(value = "select count(1) > 0 from categoria_produto where upper(trim(nome_desc)) = upper(trim(?1))", nativeQuery = true)
+    @Query(value = "select count(1) > 0 from categoria_produto where upper(trim(nome_descricao)) = upper(trim(?1))", nativeQuery = true)
     public boolean existeCategoria(String nomeCategoria);
 
     @Query("select a from CategoriaProduto a where upper(trim(a.nomeDesc)) like %?1%")
