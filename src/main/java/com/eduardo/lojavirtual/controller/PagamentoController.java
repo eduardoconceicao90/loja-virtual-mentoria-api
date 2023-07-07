f292b44161a12cd34ab758315812d7b9c0b5d8b5
Eduardo Conceição <eduardosaconceicao@gmail.com>
2023-07-07 15:23:54 -0300

    Tela de Checkout - End-Point de recebimento de dados - Parte 15

--- src/main/java/com/eduardo/lojavirtual/controller/PagamentoController.java
+++ src/main/java/com/eduardo/lojavirtual/controller/PagamentoController.java
@@ -5,10 +5,13 @@ import com.eduardo.lojavirtual.model.dto.VendaCompraLojaVirtualDTO;
 import com.eduardo.lojavirtual.repository.VendaCompraLojaVirtualRepository;
 import com.eduardo.lojavirtual.service.VendaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -33,5 +36,24 @@ public class PagamentoController {
             modelAndView.addObject("venda", vendaService.consultaVenda(compraLojaVirtual));
         }
         return modelAndView;
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "**/finalizarCompraCartao")
+    public ResponseEntity<String> finalizarCompraCartao(@RequestParam("cardHash") String cardHash,
+                                                        @RequestParam("cardNumber") String cardNumber,
+                                                        @RequestParam("holderName") String holderName,
+                                                        @RequestParam("securityCode") String securityCode,
+                                                        @RequestParam("expirationMonth") String expirationMonth,
+                                                        @RequestParam("expirationYear") String expirationYear,
+                                                        @RequestParam("idVendaCampo") Long idVendaCampo,
+                                                        @RequestParam("cpf") String cpf,
+                                                        @RequestParam("qtdparcela") Integer qtdparcela,
+                                                        @RequestParam("cep") String cep,
+                                                        @RequestParam("rua") String rua,
+                                                        @RequestParam("numero") String numero,
+                                                        @RequestParam("estado") String estado,
+                                                        @RequestParam("cidade") String cidade) throws Exception{
+
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
