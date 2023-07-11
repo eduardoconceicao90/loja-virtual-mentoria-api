03254ab60ff5da547a2127298f1e413f88678c5c
Eduardo Conceição <eduardosaconceicao@gmail.com>
2023-07-11 15:13:47 -0300

    ASAAS - Chave PIX criada e Verificando se existe cliente

--- src/main/java/com/eduardo/lojavirtual/service/ServiceAsaas.java
+++ src/main/java/com/eduardo/lojavirtual/service/ServiceAsaas.java
@@ -1,10 +1,14 @@
 package com.eduardo.lojavirtual.service;
 
+import com.eduardo.lojavirtual.model.dto.asaas.ObjetoPostCarneAssasDTO;
 import com.eduardo.lojavirtual.util.AsaasApiPagamentoStatus;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
+import org.apache.tomcat.util.json.JSONParser;
 import org.springframework.stereotype.Service;
+
+import java.util.LinkedHashMap;
 
 @Service
 public class ServiceAsaas {
@@ -23,5 +27,32 @@ public class ServiceAsaas {
         String stringRetorno = clientResponse.getEntity(String.class);
         clientResponse.close();
         return stringRetorno;
+    }
+
+    /* Retorna o id do Customer (Pessoa/cliente) */
+    public String buscaClientePessoaApiAsaas(ObjetoPostCarneAssasDTO dados) throws Exception {
+
+        /* id do cliente para ligar com a cobrança */
+        String customer_id = "";
+
+        /* --------------INICIO - criando ou consultando o cliente */
+
+        Client client = new HostIgnoringClient(AsaasApiPagamentoStatus.URL_API_ASAAS).hostIgnoringClient();
+        WebResource webResource = client.resource(AsaasApiPagamentoStatus.URL_API_ASAAS + "customers?email="+dados.getEmail());
+
+        ClientResponse clientResponse = webResource.accept("application/json;charset=UTF-8")
+                .header("Content-Type", "application/json")
+                .header("access_token", AsaasApiPagamentoStatus.API_KEY)
+                .get(ClientResponse.class);
+
+        LinkedHashMap<String, Object> parser = new JSONParser(clientResponse.getEntity(String.class)).parseObject();
+        clientResponse.close();
+        Integer total = Integer.parseInt(parser.get("totalCount").toString());
+
+        if (total <= 0) { /* Cria o cliente */
+
+        }
+
+        return customer_id;
     }
 }
--- src/test/java/com/eduardo/lojavirtual/TesteAsaas.java
+++ src/test/java/com/eduardo/lojavirtual/TesteAsaas.java
@@ -1,5 +1,6 @@
 package com.eduardo.lojavirtual;
 
+import com.eduardo.lojavirtual.model.dto.asaas.ObjetoPostCarneAssasDTO;
 import com.eduardo.lojavirtual.service.ServiceAsaas;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,5 +18,15 @@ public class TesteAsaas {
     public void testeCriarChavePixAsaas() throws Exception {
         String chaveApi = serviceAsaas.criarChavePixAsaas();
         System.out.println("Chave Asaas API: " + chaveApi);
+    }
+
+    @Test
+    public void testeBuscaClientePessoaApiAsaas() throws Exception {
+        ObjetoPostCarneAssasDTO dados = new ObjetoPostCarneAssasDTO();
+        dados.setEmail("eduardosaconceicao@gmail.com");
+        dados.setPayerName("Eduardo Conceição");
+        dados.setPayerCpfCnpj("05217956569");
+
+        serviceAsaas.buscaClientePessoaApiAsaas(dados);
     }
 }
