1ab4cf91e9a350277fd105c956b1087860830840
Eduardo Conceição <eduardosaconceicao@gmail.com>
2023-07-14 15:56:53 -0300

    ASAAS - Criando Objeto de Envio de cobrança

---
+++ src/main/java/com/eduardo/lojavirtual/model/dto/asaas/DiscontCobrancaAsaasDTO.java
@@ -0,0 +1,20 @@
+package com.eduardo.lojavirtual.model.dto.asaas;
+
+public class DiscontCobrancaAsaasDTO {
+
+    private float value;
+    private float dueDateLimitDays;
+
+    public float getValue() {
+        return value;
+    }
+    public void setValue(float value) {
+        this.value = value;
+    }
+    public float getDueDateLimitDays() {
+        return dueDateLimitDays;
+    }
+    public void setDueDateLimitDays(float dueDateLimitDays) {
+        this.dueDateLimitDays = dueDateLimitDays;
+    }
+}
---
+++ src/main/java/com/eduardo/lojavirtual/model/dto/asaas/FineCobrancaAsaasDTO.java
@@ -0,0 +1,14 @@
+package com.eduardo.lojavirtual.model.dto.asaas;
+
+public class FineCobrancaAsaasDTO {
+
+    private float value;
+
+    public void setValue(float value) {
+        this.value = value;
+    }
+
+    public float getValue() {
+        return value;
+    }
+}
