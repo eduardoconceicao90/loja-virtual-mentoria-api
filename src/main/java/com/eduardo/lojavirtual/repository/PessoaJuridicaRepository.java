a57b03848d996940990c2ee470ee07323b78f6eb
Eduardo Conceição <eduardosaconceicao@gmail.com>
2024-03-16 18:57:56 -0300

    Adicionando novos metodos no Repository Pessoa Jurídica

--- src/main/java/com/eduardo/lojavirtual/repository/PessoaJuridicaRepository.java
+++ src/main/java/com/eduardo/lojavirtual/repository/PessoaJuridicaRepository.java
@@ -1,6 +1,8 @@
 package com.eduardo.lojavirtual.repository;
 
+import com.eduardo.lojavirtual.model.Acesso;
 import com.eduardo.lojavirtual.model.PessoaJuridica;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -24,5 +26,14 @@ public interface PessoaJuridicaRepository extends JpaRepository<PessoaJuridica, 
 
     @Query(value = "select pj from PessoaJuridica pj where pj.insEstadual = ?1")
     public List<PessoaJuridica> existeInsEstadualCadastradoList(String insEstadual);
+
+    @Query("select a from PessoaJuridica a where a.empresa.id = ?1")
+    public List<PessoaJuridica> findPorPage(Long idEmpresa, Pageable pageable);
+
+    @Query(value = "SELECT cast((count(1) / 5) as integer) + 1 as qtdpagina FROM pessoa_juridica WHERE empresa_id = ?1", nativeQuery = true)
+    public Integer qdtPagina(Long idEmpresa);
+
+    @Query("select a from PessoaJuridica a where upper(trim(a.nomeFantasia)) like %?1% and a.empresa.id = ?2")
+    List<PessoaJuridica> buscarPjPorNomeFantasiaEEmpresa(String nomeFantasia, Long empresa);
 
 }
